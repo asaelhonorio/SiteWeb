9e896e3aaebf8a39bec31e61ec3df851380a43ae
Asael Honório <75455723+asaelhonorio@users.noreply.github.com>
2021-11-29 09:11:45 -0300

    coreções

--- menu.js
+++ menu.js
@@ -1,19 +1,7 @@
-// function abrirMenu(){
-//     document.getElementById('menu-lateral').style.width = '250px';
-// }
-// function fecharMenu(){
-//     document.getElementById('menu-lateral').style.width = '0px';
-// }
-// function itensMenu(){
-//     document.getElementById('menu-lateral').style.width = '0px';
-// }
-
 function abrirMenu(){
     const toggleMenu = document.querySelector('.menu-cell-lista')
     toggleMenu.classList.toggle('active')
 }
-
-
 function alerta(){
     alert("Em desenvolvimento! No momento algumas opções encontram-se indisponíveis.")
 }
