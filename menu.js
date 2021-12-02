662e95614490686af8d08a64371058234843c985
Asael Honório <75455723+asaelhonorio@users.noreply.github.com>
2021-12-02 18:59:46 -0300

    detalhes

--- menu.js
+++ menu.js
@@ -5,3 +5,9 @@ function abrirMenu(){
 function alerta(){
     alert("Em desenvolvimento! No momento algumas opções encontram-se indisponíveis.")
 }
+
+
+// ALERT INICIAL
+function alertInicial(){
+    alert("As páginas encontram-se em manutenção e estão desconfiguradas. Em breve estarão de volta!")
+}
